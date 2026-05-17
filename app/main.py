e74ad5064a2ccb184a9417d9c54953ff2de7b668
Baveet Hora <bhora@asu.edu>
2026-05-16 22:41:37 -0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -10,6 +10,15 @@ def main():
         if input == "exit":
             break
         input_list = input.split()
+
+        if input_list[0] == "type":
+            if input_list[1] == "echo":
+                sys.stdout.write(f"{input_list[1]} is a shell builtin\n")
+            elif input_list[1] == "exit":
+                sys.stdout.write(f"{input_list[1]} is a shell builtin\n")
+            else:
+                sys.stdout.write(f"{input_list[1]} not found\n")
+                
         if input_list[0] == "echo":
             sys.stdout.write(" ".join(input_list[1:]) + "\n")
         else:
