9097b0ffb1c021fe86b635d041fec952fe4e7eb1
Baveet Hora <bhora@asu.edu>
2026-05-16 23:44:49 -0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 
@@ -20,7 +21,24 @@ def main():
             elif input_list[1] == "type":
                 sys.stdout.write(f"{input_list[1]} is a shell builtin\n")
             else:
+                for dir in sys.path:
+                    try:
+                        with open(f"{dir}/{input_list[1]}") as f:
+                            ## file exists 
+                            ## checking if its executable 
+                            if os.access(f"{dir}/{input_list[1]}", os.X_OK):
+                                sys.stdout.write(f"{input_list[1]} is {dir}/{input_list[1]}\n")
+                                break
+                            else:
+                                ## file exists but is not executable
+                                continue
+                    except FileNotFoundError:
+                        ## file does not exist in this directory, check the next one
+                        continue
+                
+                ## we are here if we have checked all directories and not found the command
                 sys.stdout.write(f"{input_list[1]} not found\n")
+                
             continue
 
         if input_list[0] == "echo":
