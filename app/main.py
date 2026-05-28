95979b74b67d4011d9287fb94fa4cf55f110cbd8
Baveet Singh Hora <bhora@asu.edu>
2026-05-28 13:15:20 -0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -67,50 +67,9 @@ def main():
             sys.stdout.write("exit\n")
             break
 
-        if input.startswith("echo "):
-            raw_args = input[5:]
-            
-            result = []
-            inside_quotes = False
-            ## for double quotes 
-            if raw_args[0] == '"':
-                for i, char in enumerate(raw_args):
-                    if char == '"':
-                        # Toggle whether we are inside or outside of quotes
-                        inside_quotes = not inside_quotes
-                    elif char == " ":
-                        # If inside quotes, keep the space exactly as it is
-                        if inside_quotes:
-                            result.append(char)
-                            # If outside quotes, only add a space if the previous character wasn't a space
-                            elif result and result[-1] != " ":
-                                result.append(char)
-                else:
-                    # Keep regular characters
-                    result.append(char)
-                final_output = "".join(result).strip()
-                sys.stdout.write(final_output + "\n")
-                continue
-
-            if raw_args[0] == "'":
-                for i, char in enumerate(raw_args):
-                    if char == "'":
-                        # Toggle whether we are inside or outside of quotes
-                        inside_quotes = not inside_quotes
-                    elif char == " ":
-                        # If inside quotes, keep the space exactly as it is
-                        if inside_quotes:
-                            result.append(char)
-                            # If outside quotes, only add a space if the previous character wasn't a space
-                            elif result and result[-1] != " ":
-                                result.append(char)
-                else:
-                    # Keep regular characters
-                    result.append(char)
-                    
-                final_output = "".join(result).strip()
-                sys.stdout.write(final_output + "\n")
-                continue
+        if input_list[0] == "echo":
+            sys.stdout.write(" ".join(input_list[1:]) + "\n")
+            continue
 
         if input_list[0] == "cat":
             for file in input_list[1:]:
